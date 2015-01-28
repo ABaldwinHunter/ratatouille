9cc0ad15ce9f1d0b989f71a65618f5f44d6b137f
Ashley Baldwin-Hunter <ashleybaldwin-hunter@Ashleys-MacBook-Air.local>
2015-01-28 10:37:38 -0500

    added ingredient to recipe migration

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,5 +4,6 @@ class Recipe < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
   has_many :favorites
   has_many :fans, through: :favorites, class_name: "User"#, source: :user
+  has_many :ingredients, through: :recipe_ingredients
 
 end
