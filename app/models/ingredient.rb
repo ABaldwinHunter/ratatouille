3d704964ce9c85b5c7e01362feeceedf20069456
Ashley Baldwin-Hunter <ashleybaldwin-hunter@Ashleys-MacBook-Air.local>
2015-01-28 10:46:14 -0500

    added ingredient model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,12 @@
 class Ingredient < ActiveRecord::Base
-  # Remember to create a migration!
+  validates :name, uniqueness: true
+
+  belongs_to :type
+  has_many :recipes, through: :recipe_ingredients
+
+  has_many :substitutions
+  has_many :substitutes, through: :substitutions
+  has_many :inverse_substitutions, class_name: "Substitution", foreign_key: :substitute_id
+  has_many :inverse_substitutions, through: :substitutions, source: :ingredient
+
 end
