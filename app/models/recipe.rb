226dd2d3a6b7da4180bf63718bc032cdf3bfd630
Ashley Baldwin-Hunter <ashleybaldwin-hunter@Ashleys-MacBook-Air.local>
2015-01-28 10:36:07 -0500

    added recipe and category migrations and models

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to :category
+  belongs_to :region
+  belongs_to :creator, class_name: "User"
+  has_many :favorites
+  has_many :fans, through: :favorites, class_name: "User"#, source: :user
+
 end
--- db/migrate/20150128101235_create_recipes.rb
+++ db/migrate/20150128101235_create_recipes.rb
@@ -1,4 +1,14 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
+    create_table :recipes do |t|
+      t.references :creator
+      t.references :category
+      t.references :region
+      t.integer :rating
+      t.string :title
+      t.text :description
+      t.text :content
+      t.string :url #user can provide recipe content or link
+    end
   end
 end
