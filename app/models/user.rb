327fec050389c236e58c8c2afd611b15e49ab157
Ashley Baldwin-Hunter <ashleybaldwin-hunter@Ashleys-MacBook-Air.local>
2015-01-28 10:26:24 -0500

    create user model and add favorites migration join table so users can have favorite recipes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
-  # Remember to create a migration!
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+
+  has_many :submitted_recipes, through: :recipes, foreign_key: :creator_id
+  has_many :favorite_recipes, through: :favorites
+
 end
